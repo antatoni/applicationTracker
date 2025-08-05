2b9edad2f8238bdb34fe24f16ef4b2bbe586a9c8
Antani Kalchev <antatoni@abv.bg>
2025-08-05 17:18:00 +0300

    sort button made into selector with options

--- src/components/ApplicationBar.jsx
+++ src/components/ApplicationBar.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 const ApplicationBar = () => {
+    const [option , setOption] = useState('')
   return (
     <>
       <div className="align-center m-3 grid grid-cols-[1.2fr_0.8fr_1fr_2fr_auto] gap-8 rounded-2xl border-1 bg-[#0096C7] p-5 sm:gap-4 md:gap-6 lg:gap-8">
@@ -14,9 +17,30 @@ const ApplicationBar = () => {
         <div className="content-center overflow-hidden text-center text-xl font-bold italic hover:overflow-x-auto">
           Application URL
         </div>
-        <div className="transition-all-2ms mr-3 ml-3 content-center rounded-lg border-1 bg-[#0077B6] p-2 text-center text-xl font-bold duration-300 hover:bg-[#023E8A]">
-          Sort by
-        </div>
+        <select
+          name="Sort by"
+          value={`Sort by`}
+          className="text-md mr-5 ml-5 border-2 bg-[#0077B6] text-center font-semibold"
+        >
+          <option
+            value=""
+            className="text-md mr-5 ml-5 border-2 bg-[#0077B6] text-center font-semibold"
+          >
+            Sort by
+          </option>
+          <option
+            value="date"
+            className="text-md mr-5 ml-5 border-2 bg-[#0077B6] text-center font-semibold"
+          >
+            Sort by date
+          </option>
+          <option
+            value="stage"
+            className="text-md mr-5 ml-5 border-2 bg-[#0077B6] text-center font-semibold"
+          >
+            Sort by company
+          </option>
+        </select>
       </div>
     </>
   );
