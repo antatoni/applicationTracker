e6d5a2239d9cce8994402174869a62feef1bb23a
Antani Kalchev <antatoni@abv.bg>
2025-08-08 14:40:07 +0300

    made it into sort/filter selects

--- src/components/ApplicationBar.jsx
+++ src/components/ApplicationBar.jsx
@@ -1,7 +1,16 @@
 import { useState } from "react";
 
 const ApplicationBar = () => {
-    const [option , setOption] = useState('')
+  const [sortOption, setSortOption] = useState("");
+  const [stageOption, setStageOption] = useState("");
+  const handleSortChange = (event) => {
+    const newSort = event.target.value;
+    setSortOption(newSort);
+  };
+  const handleFilterChange = (event) => {
+    const newFilter = event.target.value;
+    setStageOption(newFilter);
+  };
   return (
     <>
       <div className="align-center m-3 grid grid-cols-[1.2fr_0.8fr_1fr_2fr_auto] gap-8 rounded-2xl border-1 bg-[#0096C7] p-5 sm:gap-4 md:gap-6 lg:gap-8">
@@ -17,30 +26,43 @@ const ApplicationBar = () => {
         <div className="content-center overflow-hidden text-center text-xl font-bold italic hover:overflow-x-auto">
           Application URL
         </div>
-        <select
-          name="Sort by"
-          value={`Sort by`}
-          className="text-md mr-5 ml-5 border-2 bg-[#0077B6] text-center font-semibold"
-        >
-          <option
-            value=""
+        <div className="flex flex-col gap-2">
+          <select
+            name="Sort by"
+            value={sortOption}
+            onChange={(e) => handleSortChange(e)}
             className="text-md mr-5 ml-5 border-2 bg-[#0077B6] text-center font-semibold"
           >
-            Sort by
-          </option>
-          <option
-            value="date"
+            <option
+              value=""
+              className="text-md mr-5 ml-5 border-2 bg-[#0077B6] text-center font-semibold"
+            >
+              Sort by
+            </option>
+
+            <option value="Newest first">Newest first</option>
+            <option value="Oldest first">Oldest first</option>
+          </select>
+          <select
+            name="Filter by"
+            value={stageOption}
+            onChange={(e) => handleFilterChange(e)}
             className="text-md mr-5 ml-5 border-2 bg-[#0077B6] text-center font-semibold"
           >
-            Sort by date
-          </option>
-          <option
-            value="stage"
-            className="text-md mr-5 ml-5 border-2 bg-[#0077B6] text-center font-semibold"
-          >
-            Sort by company
-          </option>
-        </select>
+            <option
+              value=""
+              className="text-md mr-5 ml-5 border-2 bg-[#0077B6] text-center font-semibold"
+            >
+              Filter by
+            </option>
+            <option value="Applied">Applied</option>
+            <option value="Rejected">Rejected</option>
+            <option value="Homework">Homework</option>
+            <option value="First interview">First interview</option>
+            <option value="Second interview">Second interview</option>
+            <option value="Got the job!">Got the job!</option>
+          </select>
+        </div>
       </div>
     </>
   );
