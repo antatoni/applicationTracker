f2c9370697809288824f715708dbd9d38388eb7c
Antani Kalchev <antatoni@abv.bg>
2026-04-20 20:51:37 +0300

    hecking db missmatch now

---
+++ backend/ApplicationTracker.Api/Migrations/20260420173744_RenameUserUidToUserId.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApplicationTracker.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameUserUidToUserId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserUid",
+                table: "Applications",
+                newName: "userId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Applications_UserUid",
+                table: "Applications",
+                newName: "IX_Applications_userId");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AppliedOn",
+                table: "Applications",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "userId",
+                table: "Applications",
+                newName: "UserUid");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Applications_userId",
+                table: "Applications",
+                newName: "IX_Applications_UserUid");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AppliedOn",
+                table: "Applications",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+    }
+}
